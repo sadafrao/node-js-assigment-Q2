cc6a9aa2e9d0fbe3999b959b893d25db0c2ca664
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 12:37:43 -0800

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,7 @@
+"use strict";
+// 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+// let name2: string ="hello Eric would you like to learn some python today?"
+// console.log(name2)
+let name2 = "Eric";
+console.log(`Hello ${name2},"would you like to learn some python today?"`);
---
+++ Q2.ts
@@ -0,0 +1,6 @@
+// 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+// let name2: string ="hello Eric would you like to learn some python today?"
+// console.log(name2)
+let name2: string = "Eric"
+console.log (`Hello ${name2},"would you like to learn some python today?"`)
